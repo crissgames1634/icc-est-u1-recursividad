a43c907629f47b68b420fb2d981fd742e07ccfbd
crissgames1634 <95445371+crissgames1634@users.noreply.github.com>
2025-04-09 16:58:34 -0500

    Add files via upload

---
+++ icc-est1-u1-/icc-est1-u1-/src/EjerciciosRecursivos.java
@@ -0,0 +1,20 @@
+public class EjerciciosRecursivos {
+    public int fibonacci(int n){
+        if(n <=1){
+            return n;
+        }
+
+        return fibonacci(n-1)+ fibonacci(n-2);
+    }
+    public int sumaConsecutivos(int n){
+        if(n == 1){
+            return 1;
+        }
+        int resultadoParcial = sumaConsecutivos(n-1);
+        int resultado = n + resultadoParcial;
+        return resultado;
+        //return n + sumaConsecutivos( n-1);
+
+    }
+
+}
